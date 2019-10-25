369592d1b112189f16bfdc795b957a35753ce2d8
Danny Ben Shitrit <db@dannyben.com>
2019-10-25 12:53:01 +0000

    simplify approvals folder

--- spec/extended_yaml/extended_yaml_spec.rb
+++ spec/extended_yaml/extended_yaml_spec.rb
@@ -6,13 +6,13 @@ describe ExtendedYAML do
     subject { ExtendedYAML.load 'examples/master.yml' }
 
     it "loads and evaluates a YAML file" do
-      expect(subject.to_yaml).to match_fixture('examples/master.yml')
+      expect(subject.to_yaml).to match_fixture('master.yml')
     end
 
     context "when a different key is provided" do
       subject { ExtendedYAML.load 'examples/different_key.yml', key: 'include' }
       it "uses that key to load additional YAML files" do
-        expect(subject.to_yaml).to match_fixture('examples/different_key.yml')
+        expect(subject.to_yaml).to match_fixture('different_key.yml')
       end
     end
   end
