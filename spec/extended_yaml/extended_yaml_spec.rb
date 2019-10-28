384ff6816b344539a89749775a8b89fe7ef1ed8a
Danny Ben Shitrit <db@dannyben.com>
2019-10-28 16:02:59 +0000

    add support for extending with wildcard

--- spec/extended_yaml/extended_yaml_spec.rb
+++ spec/extended_yaml/extended_yaml_spec.rb
@@ -3,16 +3,25 @@ require 'yaml'
 
 describe ExtendedYAML do
   describe '::load' do
-    subject { ExtendedYAML.load 'examples/master.yml' }
+    subject { described_class.load 'examples/master.yml' }
 
     it "loads and evaluates a YAML file" do
       expect(subject.to_yaml).to match_fixture('master.yml')
     end
 
     context "when a different key is provided" do
-      subject { ExtendedYAML.load 'examples/different_key.yml', key: 'include' }
+      subject { described_class.load 'examples/different_key.yml', key: 'include' }
+
       it "uses that key to load additional YAML files" do
         expect(subject.to_yaml).to match_fixture('different_key.yml')
+      end
+    end
+
+    context "when using wildcards" do
+      subject { described_class.load 'examples/wildcard.yml' }
+
+      it "imports all files" do
+        expect(subject.to_yaml).to match_fixture('wildcard.yml')
       end
     end
   end
